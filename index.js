7b036c31d770043f94d569b19d7302b88fa1bf22
Vladimir Kudinov <frux@yandex-team.ru>
2016-02-13 00:15:28 +0500

    Still debigging

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Gerkon = require('gerkon');
 const app = new Gerkon();
 console.log('Up!');
-app.get('/', (req, res) => { console.log(req);
-		res.writeHead({ 'Content-type': 'text/html' });
-		res.write('Hello world!');
-		res.end();	
+app.get('/', (req, res) => { console.log(res);
+		res.end('Hello World!');	
 	});
 
 app.listen(process.env.PORT || 3163);
