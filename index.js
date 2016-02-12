ebae79440235899ca188eb284279dd6b1aa93aca
Vladimir Kudinov <frux@yandex-team.ru>
2016-02-13 00:18:33 +0500

    Remove console.log

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Gerkon = require('gerkon');
 const app = new Gerkon();
-console.log('Up!');
-app.get('/', (req, res) => { console.log(res);
+
+app.get('/', (req, res) => {
 		res.end('Hello World!');	
 	});
 
 app.listen(process.env.PORT || 3163);
+console.log(`Application has started on localhost:${process.env.PORT || 3163}`);
